0c38e486560fcb5f3ed282c8cddd353906aebbf6
Braden-Kapp <braden.h.kapp@gmail.com>
2026-02-04 20:57:57 -0600

    Adding starting files to github

---
+++ mini_game_select.gd
@@ -0,0 +1,49 @@
+extends Node2D
+#STYLE NODES GET CAMEL CASE ALL ELSE USE _SEPERATION
+@onready var minigame_container = $minigameContainer
+var current_minigame: Node = null
+
+func load_minigame(path: String):
+	# Potentially Remove the current minigame
+	# Shouldn't be needed
+	if current_minigame:
+		current_minigame.queue_free()
+
+	var packed_minigame := load(path) as PackedScene
+	if packed_minigame == null:
+		#error checkin
+		push_error("Failed to load minigame: " + path)
+		return
+
+	current_minigame = packed_minigame.instantiate()
+	minigame_container.add_child(current_minigame)
+	#add the finish signal
+	if current_minigame.has_signal("finished"):
+		current_minigame.finished.connect(_on_minigame_finished)
+	
+#Calls Minigame on start
+func _ready() -> void:
+	load_minigame("res://test.tscn") #currently used for testing
+#PackedScene switch to preload PackedScene here and send it to function
+func _on_minigame_finished():
+	print("Game finished")
+	#remove and free the cur minigame
+	current_minigame.queue_free()
+	current_minigame = null
+	
+#ADD TO ALL MINIGAMES
+#signal finished
+
+#func end_game():
+#    emit_signal("finished")
+#    queue_free()
+
+#To edit vars in future
+#current_minigame.score
+#To access Nodes
+#if current_minigame.has_node("Timer"):
+#	current_minigame.get_node("Timer").wait_time = 5.0
+
+#cannot access:
+#@onready variables before _ready() 
+#local variables  
